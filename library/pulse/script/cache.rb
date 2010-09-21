719181900fbb933b7935757932867594869aa85c
Mikkel Kroman <mk@maero.dk>
2010-09-21 14:35:20 +0200

    Fixed namespacing of script cache and corrected Cache#path

--- library/pulse/script/cache.rb
+++ library/pulse/script/cache.rb
@@ -1,14 +1,16 @@
 # encoding: utf-8
 
 module Pulse
-  class Cache
-    def initialize script
-      @script = script
-    end
+  class Script
+    class Cache
+      def initialize script
+        @script = script
+      end
 
-  private
-    def path
-      File.expand_path $0
+    private
+      def path
+        "#{File.expand_path $0}/cache/#{@script.name}_#{@index}"
+      end
     end
   end
 end
