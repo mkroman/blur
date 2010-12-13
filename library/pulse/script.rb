e0f0b4389d0651b8bbc78153ed805dc4ae6cde94
Mikkel Kroman <mk@maero.dk>
2010-12-14 00:40:20 +0100

    Reset library/pulse/script.rb

--- library/pulse/script.rb
+++ library/pulse/script.rb
@@ -2,37 +2,48 @@
 
 module Pulse
   class Script < Module
+
     attr_accessor :name, :author, :version, :path
 
     def initialize path, client
-      @path, @client, @loaded = path, client, false
+      @path, @client = path, client
+      @loaded = false
 
+      evaluate_script
       cache.load if has_cache?
-      emit :loaded if evaluate_script
+      __send__ :loaded if respond_to? :loaded
     end
 
-    def Script name, version = [1,0,0], author = nil, &block
+    def Script name, version = [1, 0, 0], author = nil, &block
       @name, @version, @author = name, version, author
-
       instance_eval &block
     end
 
-    def cache_for name; script = @client.scripts.find { |script| script.name == name } ? script.cache : nil end
-
-    def emit method, *args; __send__ method, *args if respond_to? method end
-    def script name; @client.scripts.find { |script| script.name == name } end
-    def cache; @cache ||= Cache.new self end
-
-    def has_cache?; File.exists? "#{File.expand_path File.dirname $0}/cache/#@name.yml" end
     def loaded?; @loaded == true end
 
-    def unload!; cache.save if @cache; emit :unloaded end
+    def unload!
+      cache.save if @cache
+      __send__ :unloaded if respond_to? :unloaded
+    end
 
     def to_yaml opts = {}; @name.to_yaml opts end
 
     def inspect
       %{#<#{self.class.name} @name=#{@name.inspect} @version=#{@version.inspect} @author=#{@author.inspect}}
     end
+
+    def cache_for name
+      script = @client.scripts.find { |script| script.name == name }
+      script ? script.cache : nil
+    end
+
+    def script name; @client.scripts.find { |script| script.name == name } end
+
+    def has_cache?
+      File.exists? "#{File.expand_path File.dirname $0}/cache/#@name.yml"
+    end
+
+    def cache; @cache ||= Cache.new self end
 
   private
 
@@ -41,5 +52,6 @@ module Pulse
     rescue
       puts "Parse error: #{$!.message}"
     end
+
   end
 end
