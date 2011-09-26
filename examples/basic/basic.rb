caf440729326205a4efc17ce081a4625eef571f3
Mikkel Kroman <mk@maero.dk>
2011-09-26 23:35:19 +0200

    Wrote documentation for the basic example.

--- examples/basic/basic.rb
+++ examples/basic/basic.rb
@@ -3,6 +3,10 @@
 
 require 'blur'
 
+# Set our options.
+#
+# @see Blur::Network.new
+# @see Blur::Client.new
 options = {
   networks: [{
      hostname: "uplink.io",
@@ -11,17 +15,23 @@ options = {
   }]
 }
 
+# Start connecting to all our networks.
+#
+# @see Blur.connect
 Blur.connect options do
+  # Raised once for every network upon successful connection.
   catch :connection_ready do |network|
     puts "Connection and established and I'm ready for action!"
   end
 
+  # Raised when a message is sent from inside a channel.
   catch :message do |user, channel, message|
     print Time.now.strftime("%I:%M:%S") + " "
     print "#{channel}:#{user}: "
     puts message
   end
 
+  # Raised when a message is sent from a user outside of a channel.
   catch :private_message do |user, message|
     user.say "Sorry, I do not handle personal business!"
   end
