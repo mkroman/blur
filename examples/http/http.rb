65fee7415481aeb0a0ed5e1f13b269f663778eb0
Mikkel Kroman <mk@uplink.io>
2014-03-17 22:47:25 +0100

    Add http script and extension example

---
+++ examples/http/http.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+$:.unshift File.dirname(__FILE__) + '/../../library'
+require 'blur'
+
+# Set our options.
+#
+# @see Blur::Network.new
+# @see Blur::Client.new
+options = {
+  networks: [{
+     hostname: "uplink.io",
+     nickname: "basic",
+     channels: %w{#channel #channel2}
+  }]
+}
+
+# Start connecting to all our networks.
+#
+# @see Blur.connect
+Blur.connect options do
+  # Raised once for every network upon successful connection.
+  catch :connection_ready do |network|
+    log.info "Connection established and I'm ready for action!"
+  end
+end
