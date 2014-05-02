454ffab9192d90379287834ffbc1430d8d5a6825
Mikkel Kroman <mk@uplink.io>
2014-05-02 22:21:00 +0200

    Added deferrable

---
+++ library/blur/deferrable.rb
@@ -0,0 +1,41 @@
+# encoding: utf-8
+
+module Blur
+  module Deferrable
+    # Include the mixin.
+    def self.included base
+      base.send :include, InstanceMethods
+    end
+
+    module InstanceMethods
+      # Return the list of callbacks registered.
+      #
+      # If the callbacks stack isn't already initialized, then initialize it.
+      def callbacks
+        @callbacks ||= {}
+      end
+
+      # Emit a new event with given arguments.
+      #
+      # @param name [Symbol] The event name.
+      # @param args [optional, Array] The list of arguments to pass.
+      def emit name, *args
+        callbacks = self.callbacks[name]
+
+        return unless callbacks
+
+        callbacks.each do |callback|
+          callback.(*args)
+        end if callbacks.any?
+      end
+
+      # Add a new event callback.
+      #
+      # @param name [Symbol] The event name.
+      # @yield [args, ...] The arguments passed from #emit.
+      def on name, &block
+        (callbacks[name] ||= []) << block
+      end
+    end
+  end
+end
