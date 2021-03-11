af83ed56a47be5a7e3a1b4542653fd4d53eac6cb
Mikkel Kroman <mk@maero.dk>
2021-03-11 09:14:59 +0100

    Fix logging crash on runtime error

--- library/blur/client.rb
+++ library/blur/client.rb
@@ -77,7 +77,7 @@ module Blur
           backtrace = exception.backtrace.first
           error_line = backtrace.match(message_pattern)[1].to_i + 1
 
-          log.error "#{exception.message ^ :bold} on line #{error_line.to_s ^ :bold}"
+          puts "#{exception.message ^ :bold} on line #{error_line.to_s ^ :bold}"
           puts exception.backtrace.join "\n"
         end
       end
